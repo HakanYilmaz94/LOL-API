4023bf0a0bcb082d188713a977b529e322d38653
hkn94 <hkn94@192.168.1.20>
2018-12-08 20:42:00 +0300

    web controller added

---
+++ src/main/java/com/lol/web/controller/BillingController.java
@@ -0,0 +1,20 @@
+package com.lol.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class BillingController {
+
+	@GetMapping(value = "/views/checkUser")
+	public String getBillingView(
+			@RequestParam(name = "username", required = true, defaultValue = "") String username,
+			Model model) {
+
+		model.addAttribute("username",username);
+		return "firstPage";
+	}
+
+}
