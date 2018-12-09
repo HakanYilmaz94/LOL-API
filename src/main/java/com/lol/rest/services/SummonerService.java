b018763562961d34f837043cbca4beb1e6ec4565
Soner <Soner@Soner-PC>
2018-12-09 03:57:40 +0300

    refactoring and created generic request function

---
+++ src/main/java/com/lol/rest/services/SummonerService.java
@@ -0,0 +1,26 @@
+package com.lol.rest.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.lol.dto.SummonerDTO;
+import com.lol.util.LolConstants;
+
+@Service
+public class SummonerService {
+
+	@Value("${lol.api.key}")
+	private String apiKey;
+
+	@Autowired
+	CommonService commonService;
+
+	public SummonerDTO getSummoners(String username) {
+
+		String request = LolConstants.SUMMONERS_API + username + "?api_key=" + apiKey;
+		SummonerDTO summoner = (SummonerDTO) commonService.getResponse(request, SummonerDTO.class);
+		return summoner;
+
+	}
+}
---
+++ src/main/java/com/lol/util/LolConstants.java
@@ -0,0 +1,7 @@
+package com.lol.util;
+
+public class LolConstants {
+	
+	public static String SUMMONERS_API = "https://tr1.api.riotgames.com/lol/summoner/v3/summoners/by-name/";
+
+}
