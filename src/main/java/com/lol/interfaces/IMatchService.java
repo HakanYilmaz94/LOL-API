9830157e5e4d4c35454b34602816dcb2ca0020c3
Soner <Soner@Soner-PC>
2019-01-12 20:47:41 +0300

    refactoring and missing services added.

--- src/main/java/com/lol/interfaces/IMatchService.java
+++ src/main/java/com/lol/interfaces/IMatchService.java
@@ -1,9 +1,12 @@
 package com.lol.interfaces;
 
 import com.lol.dto.MatchDTO;
+import com.lol.dto.MatchListDTO;
 
 public interface IMatchService {
 
 	MatchDTO getMatchInfoByMatchId(Long matchId);
+
+	MatchListDTO getMatchListsByAccoundID(String accountID);
 
 }
